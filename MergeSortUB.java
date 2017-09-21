ad298120a86a330d8e4559b582ba9cdc909902da
aesne <aesne@users.noreply.github.com>
2017-09-21 14:58:31 +0800

    up bottom merge sort

---
+++ MergeSortUB.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class MergeSortUB{
+	private static int[] temp;
+	
+	public static void sort(int[] a){
+		temp = new int[a.length];
+		sort(a, 0, a.length - 1);
+	}
+	
+	private static void sort(int[] a, int lo, int hi){
+		if(hi <= lo){
+			return;
+		}
+		int mid = lo + (hi - lo) / 2;
+		sort(a, lo, mid);
+		sort(a, mid + 1, hi);
+		merge(a, lo, mid, hi);
+	}
+	
+	private static void merge(int[] a, int lo, int mid, int hi){
+		for(int k = lo; k <= hi; k++){
+			temp[k] = a[k];
+		}
+		int i = lo, j = mid + 1;
+		for(int k = lo; k <= hi; k++){
+			if(i > mid){
+				a[k] = temp[j++];
+			}else if(j > hi){
+				a[k] = temp[i++];
+			}else if(temp[j] < temp[i]){
+				a[k] = temp[j++];
+			}else{
+				a[k] = temp[i++];
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		int[] a = {2, 5, 8, 4, 9, 0, 6, 7, 1, 3};
+        MergeSortUB.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
